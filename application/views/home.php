a958ff73d25390664ebbcaa69068bcb946c3ba3e
Candice Ballarta <candiceballarta@gmail.com>
2020-06-15 23:19:28 +0800

    login page and application

--- application/views/home.php
+++ application/views/home.php
@@ -3,8 +3,21 @@
         <div class="col-xs-12"></div>
     </div>
     <div class="row">
-        <div class="col-md-6">Hello</div>
-        <div class="col-md-6">World</div>
+        <div class="col-md-6">
+            <form action="/login.php" method="post">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+                <input type="checkbox" name="remember" id="remember">
+                <label for="remember">Remember Me?</label>
+                
+                <input type="button" value="LogIn">
+            </form>
+        </div>
+        <div class="col-md-6">
+        
+        </div>
     </div>
     <div class="row">
         <div class="col-xs-12"></div>
---
+++ application/views/student_create_application.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <form action="/application.php" method="post">
+        
+            </form>
+        </div>
+        <div class="col-md-6">
+        
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12"></div>
+    </div>
+</div>
